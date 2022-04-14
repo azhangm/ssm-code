04a24c415acef0d22ba412b104013333e35a5543
dajuancai <742824624@qq.com>
2022-04-14 16:54:56 +0800

    first commit spring 初步整合 mybatis

--- ssm-parent/ssm-service/src/main/java/HelloTest.java
+++ ssm-parent/ssm-service/src/main/java/HelloTest.java
@@ -1,23 +1,19 @@
-import com.dajuancai.dao.TestMapper;
 import com.dajuancai.pojo.Account;
 import com.dajuancai.service.TestService;
-import com.dajuancai.service.impl.TestServiceImpl;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.List;
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration({"classpath:springconfig-service.xml"})
-public class TestF {
+
+public class HelloTest {
 
     @Autowired
-    private TestService testService;
+    private  TestService testService;
 
-    @Test
-    public void findAll(){
+@org.junit.Test
+    public void test(){
         List<Account> all = testService.findAll();
         for (Account account : all) {
             System.out.println(account);
--- ssm-parent/ssm-service/src/main/java/com/dajuancai/service/impl/TestServiceImpl.java
+++ ssm-parent/ssm-service/src/main/java/com/dajuancai/service/impl/TestServiceImpl.java
@@ -4,6 +4,7 @@ import com.dajuancai.dao.TestMapper;
 import com.dajuancai.pojo.Account;
 import com.dajuancai.service.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,7 +12,7 @@ import java.util.List;
 public class TestServiceImpl implements TestService {
 
     @Autowired
-   private  TestMapper mapper;
+   public   TestMapper mapper;
 
     @Override
     public List<Account> findAll() {
